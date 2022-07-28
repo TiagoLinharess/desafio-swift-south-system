2264cc4eb5a1c657d52c72f975ef14207aa8877d
Tiago Linhares <tiagolinharessouza@gmail.com>
2022-07-28 17:13:40 -0300

    Cria viewData e cell

---
+++ Eventos/Eventos/Models/Events/EventViewData.swift
@@ -0,0 +1,18 @@
+//
+//  EventViewData.swift
+//  Eventos
+//
+//  Created by Tiago Linhares on 28/07/22.
+//
+
+import Foundation
+
+struct EventViewData {
+    
+    let id: String
+    let title: String
+    let price: Double
+    let image: String
+    let description: String
+    let date: Double
+}
---
+++ Eventos/Eventos/Ui/Events/EventsList/Scenes/EventsListTableViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  EventsListTableViewCell.swift
+//  Eventos
+//
+//  Created by Tiago Linhares on 28/07/22.
+//
+
+import UIKit
+
+class EventsListTableViewCell: UITableViewCell {
+    
+}
